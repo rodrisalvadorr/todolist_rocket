1475d15782463f71acd50d2a80e851e6a7ebd83d
rodrisalvadorr <rodrigoagamer@gmail.com>
2023-10-10 13:31:29 -0300

    feat: database integration

---
+++ src/main/java/com/rodrigosalvador/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigosalvador.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUserName(String userName);
+}
