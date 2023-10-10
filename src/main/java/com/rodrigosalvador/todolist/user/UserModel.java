9aedf57375836966bfb01610cb029e209d763433
rodrisalvadorr <rodrigoagamer@gmail.com>
2023-10-10 11:39:31 -0300

    initial commit

---
+++ src/main/java/com/rodrigosalvador/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package com.rodrigosalvador.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String userName;
+    private String name;
+    private String password;
+
+}
